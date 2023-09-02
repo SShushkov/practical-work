ddf7801e2a9c5d72e4cc4563782cfa1d363f6670
Sergey <greyinventor@gmail.com>
2023-09-02 19:41:44 +0300

    fix(module-12): fix magic numbers

--- Module-12/PracticalWork/Task-4/main.cpp
+++ Module-12/PracticalWork/Task-4/main.cpp
@@ -11,14 +11,16 @@ using std::endl;
 
 int main()
 {
-  int numbers[15] = {114, 111, 106, 107, 108, 105, 115, 108, 110, 109, 112, 113, 116, 117, 118};
+  int numbers[] = {114, 111, 106, 107, 108, 105, 115, 108, 110, 109, 112, 113, 116, 117, 118};
   int sumNumbers = 0;
   int sumNumbersAll = 0;
   int minNum = 1000000;
 
   cout << "\"Sequence of numbers\"" << endl;
 
-  for (int i = 0; i < 15; i++)
+  int coutNum = std::size(numbers); // Переменная определяет количество элементов в массиве
+
+  for (int i = 0; i < coutNum; i++)
   {
     if (minNum > numbers[i])
     {
@@ -26,15 +28,15 @@ int main()
     }
     sumNumbersAll += numbers[i];
   }
-  sumNumbers = (14 * (minNum + (minNum + 13))) / 2; // Формула Гауса
- 
+  sumNumbers = ((coutNum - 1) * (minNum + (minNum + coutNum - 2))) / 2; // Формула Гауса
+
   if (sumNumbers > sumNumbersAll)
   {
     cout << "Repeating number: " << (sumNumbers - sumNumbersAll) - 1;
   }
   else
   {
-    cout << "Repeating number: " << (sumNumbersAll - sumNumbers) ;
+    cout << "Repeating number: " << (sumNumbersAll - sumNumbers);
   }
 
   std::cin.ignore();
